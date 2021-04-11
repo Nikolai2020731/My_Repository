b0868ad91b8cd3e5b0df21f215c07272380ddb8d
Nikolai2020731 <nikolai.ruslyakov@yandex.ru>
2021-04-11 21:59:22 +0500

    изменил текст вывода

--- sandbox/src/main/java/ru/stqa/pft/sandbox/FirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/FirstProgram.java
@@ -2,7 +2,7 @@ package ru.stqa.pft.sandbox;
 
 public class FirstProgram {
 	public static void main (String[] args){
-		System.out.println("So simple program... I have done it!");
+		System.out.println("Let's do it");
 	
 
 	}
