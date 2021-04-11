bc9e70c532e6f8e13ca821ada9aedd882e67660f
Nikolai2020731 <nikolai.ruslyakov@yandex.ru>
2021-04-11 22:15:26 +0500

    reformat code

--- sandbox/src/main/java/ru/stqa/pft/sandbox/FirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/FirstProgram.java
@@ -1,12 +1,11 @@
 package ru.stqa.pft.sandbox;
 
 public class FirstProgram {
-	public static void main (String[] args){
-		System.out.println("Let's do it");
-	
+    public static void main(String[] args) {
+        System.out.println("Let's do it");
 
-	}
 
+    }
 
 
 }
