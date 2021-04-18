bd9c0f391a7909eb42125a2d11925d92f04f7876
Nikolai2020731 <nikolai.ruslyakov@yandex.ru>
2021-04-19 01:39:35 +0500

    Задание №6. Добавил классы для редактирования групп и контактов
    GroupModificationTests и ContactModificationTests

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactModificationTests extends TestBase {
+
+    @Test
+    public void testContactModification () {
+        app.getGroupHelper().selectGroup();
+        app.getContactHelper().initContactModification();
+        app.getContactHelper().fillContactForm(new ContactData("NIKOLAY", "RUSLYAKOV", "KOLYA", "Alfa-bank", "Ekaterinburg", "89123065091", "kolya.ruslyakov@mail.ru"));
+        app.getContactHelper().submitContactModification();
+        app.getContactHelper().returnToHome();
+
+    }
+
+}
