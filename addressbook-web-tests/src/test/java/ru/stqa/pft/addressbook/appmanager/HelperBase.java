5003b4b4e10ec04eae4811cf5daa6a11509b1a21
Nikolai2020731 <nikolai.ruslyakov@yandex.ru>
2021-04-25 21:43:13 +0500

    Лекция 3.5. Оптимизация заполнения форм: оставляем дефолтные значения в полях
    Применено для GroupCreationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -17,8 +17,11 @@ public class HelperBase {
 
     public void type(By locator, String text) {
         wb.findElement(locator).click();
-        wb.findElement(locator).clear();
-        wb.findElement(locator).sendKeys(text);
+        if (text != null) {
+            wb.findElement(locator).clear();
+            wb.findElement(locator).sendKeys(text);
+        }
+
     }
 
     public boolean isAlertPresent() {
