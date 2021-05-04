684c0a42dd302624cae2346f1425e1bd96c4e5df
Nikolai2020731 <nikolai.ruslyakov@yandex.ru>
2021-05-04 22:35:22 +0500

    Лекция 4.5. Формирование коллекции
    Для контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -2,9 +2,14 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContactHelper extends HelperBase {
 
@@ -45,8 +50,9 @@ public class ContactHelper extends HelperBase {
         click(By.linkText("add new"));
     }
 
-    public void selectContact() {
-        wb.findElement(By.name("selected[]")).click();
+    public void selectContact(int index) {
+        wb.findElements(By.name("selected[]")).get(index).click();
+
 
     }
 
@@ -81,5 +87,18 @@ public class ContactHelper extends HelperBase {
     public int getContactCount() {
         return wb.findElements(By.name("selected[]")).size();
 
+    }
+
+    public List<ContactData> getContactList() {
+        List<ContactData> contacts = new ArrayList<ContactData>();
+        List<WebElement> elements = wb.findElements(By.cssSelector("tr[name=\"entry\"]"));
+        for (WebElement element:elements) {
+            String LastName = element.findElement(By.cssSelector("td:nth-child(2)")).getText();
+            String FirstName = element.findElement(By.cssSelector("td:nth-child(3)")).getText();
+            //String id = element.findElement(By.tagName("input")).getAttribute("value");
+            ContactData contact = new ContactData(FirstName, LastName, null, null,null,null,null,null);
+            contacts.add(contact);
+        }
+        return contacts;
     }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -4,22 +4,24 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.List;
+
 
 public class ContactModificationTests extends TestBase {
 
     @Test
     public void testContactModification () {
-        int before = app.getContactHelper().getContactCount();
         if (! app.getContactHelper().isThereAContact()) {
             app.getContactHelper().createContact(new ContactData("Nikolay", "Ruslyakov", "kolya", "Alfa-bank", "Ekaterinburg", "89123065091", "kolya.ruslyakov@mail.ru", "test1"), true);
         }
-        app.getContactHelper().selectContact();
+        List<ContactData> before = app.getContactHelper().getContactList();
+        app.getContactHelper().selectContact(before.size()-1);
         app.getContactHelper().initContactModification();
         app.getContactHelper().fillContactForm(new ContactData("NIKOLAY", "RUSLYAKOV", "KOLYA", "Alfa-bank", "Ekaterinburg", "89123065091", "kolya.ruslyakov@mail.ru", null), false);
         app.getContactHelper().submitContactModification();
         app.getContactHelper().returnToHome();
-        int after = app.getContactHelper().getContactCount();
-        Assert.assertEquals(after,before);
+        List<ContactData> after = app.getContactHelper().getContactList();
+        Assert.assertEquals(after.size(),before.size());
     }
 
 }
